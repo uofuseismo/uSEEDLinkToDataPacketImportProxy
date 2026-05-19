c1dc09d617c8de8777aa67c398bb044c5bacac93
Ben Baker <bakerb845@gmail.com>
2026-05-19 09:26:51 -0600

    adding git tag information to version

--- src/version.cpp
+++ src/version.cpp
@@ -18,8 +18,10 @@ int Version::getPatch() noexcept
     return uSEEDLinkToDataPacketImportProxy_PATCH;
 }
 
+//NOLINTBEGIN(bugprone-easily-swappable-parameters)
 bool Version::isAtLeast(const int major, const int minor,
                         const int patch) noexcept
+//NOLINTEND(bugprone-easily-swappable-parameters)
 {
     if (uSEEDLinkToDataPacketImportProxy_MAJOR < major){return false;}
     if (uSEEDLinkToDataPacketImportProxy_MAJOR > major){return true;}
@@ -29,9 +31,29 @@ bool Version::isAtLeast(const int major, const int minor,
     return true;
 }
 
+std::string Version::getTag() noexcept
+{
+    std::string tag{uSEEDLinkToDataPacketImportProxy_GITTAG};
+    return tag;
+}
+
+std::string Version::getVersionWithTag() noexcept
+{
+    auto tag = Version::getTag();
+    if (tag.empty())
+    {
+        return Version::getVersion();
+    }
+    else
+    {
+        return Version::getVersion() + "-" + tag;
+    }
+}
+
 std::string Version::getVersion() noexcept
 {
     std::string version{uSEEDLinkToDataPacketImportProxy_VERSION};
     return version;
 }
+
 
